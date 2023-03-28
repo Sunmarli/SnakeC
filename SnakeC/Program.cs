718fbcf0a3954cd748d7ca2be6f118fb65f25d63
Maria Jarv <maria.julia.jarv@gmail.com>
2023-03-28 08:29:31 +0300

    Add project files.

---
+++ SnakeC/Program.cs
@@ -0,0 +1,28 @@
+﻿using SnakeC;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {   
+            Point p1 = new Point(1,3,'*');
+            /*p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';*/
+            p1.Draw();
+
+            Point p2 = new Point(4,5,'$');
+            p2.Draw();
+
+            HorizontalLine Line = new HorizontalLine(5,10,8,'+');
+            Line.Drow();
+
+
+
+            Console.ReadLine();
+        }
+        }
+    }
